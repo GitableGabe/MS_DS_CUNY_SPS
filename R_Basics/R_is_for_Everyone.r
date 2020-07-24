d40c8c6dcfd32e85bcab87e855735a480370fa3b
gc4779 <campos.andre.xyz@gmail.com>
2020-07-24 10:05:27 -0400

    Create R_is_for_Everyone.r

---
+++ R_Basics/R_is_for_Everyone.r
@@ -0,0 +1,59 @@
+# R is for Everyone
+# Commands
+
+#Basics of R
+1+1
+1+2+3
+3*7*2
+4/2
+4/3
+4*6+5
+(4*6)+5
+4*(6+5)
+x<-2
+x
+y = 5
+y
+3-> z
+z
+a<- b <- 7
+a
+b
+assign("j",4)
+j
+rm(j)
+j
+theVariable <- 17
+theVariable
+THEVARIABLE
+class(x)
+is.numeric(x)
+i <-5L
+i
+is.integer(i)
+is.numeric(i)
+class(4L)
+class(2.8)
+4L * 2.8
+class(4L * 2.8)
+class(5L)
+class(2L)
+5L/2L
+class (5L/2L)
+x<- "data"
+x
+y<- factor ("data")
+y
+nchar(x)
+nchar("hello")
+nchar (3)
+nchar (452)
+nchar (y)
+date1 <- as.Date("2012-06-28")
+date1
+class(date1)
+as.numeric(date1)
+date2<- as.POSIXct("2012-06-28 17:42")
+date2
+class(date2)
+as.numeric(date2)
